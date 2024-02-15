49ee98c38c97764500192774540330467ad3084d
asrorxoja07 <asrorxojaisomiddinov21@gmail.com>
2024-02-15 22:31:15 +0500

    vazifa bajarildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 
-
+import './index.css'
 
 function App() {
  
@@ -29,7 +29,7 @@ function AddItem(){
   setItems( oldList=>[...oldList, item]);
   
   setNewItem('');
-  console.log(items);
+ 
 }
 
 function deleteItem(id){
@@ -56,10 +56,11 @@ function deleteItem(id){
      <div className="block">
       
       <h1>All Tasks</h1>
-      <input className='input' onChange={ e => setNewItem(e.target.value)} value={newItem} type="text" placeholder='Add a new task insdie ‘All’ category'/>
+      <input className='input' onChange={e => setNewItem(e.target.value)} value={newItem} type="text" placeholder='Add a new task insdie ‘All’ category'/>
   <button id='button' onClick={()=> AddItem()}>Add</button>
     <div className="table">
       <ul>
+        
         {items.map(item =>{
         return(
           <li key={item.id}>{item.value} 
@@ -83,26 +84,6 @@ function deleteItem(id){
   )
 }
 
-export default App
+export default App;
 
 
-
-// // App.js
-// import { useState } from "react";
-// import './App.css';
-// function App() {
-//     const [key, setkey] = useState();
-//     const keyUp = (event) => {
-//         setkey(event.key)
-//     }
-//     return (
-//         <div className="App">
-//             <h1>GeeksforGeeks</h1>
-//             {key ? <h2>Key UP : {key}</h2> : null}
-//             <input type='text'
-//                    onKeyUp={keyUp} 
-//                    placeholder='Press & Release Key here...' />
-//         </div>
-//     );
-// }
-// export default App;
